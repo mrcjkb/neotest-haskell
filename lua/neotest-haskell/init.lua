089769fb6f387aaa639c162ce36aad74a6bea93d
Marc Jakobi <marc.jakobi@tiko.energy>
2022-08-12 15:00:47 +0200

    Update treesitter query with qualified function calls

--- lua/neotest-haskell/init.lua
+++ lua/neotest-haskell/init.lua
@@ -24,23 +24,38 @@ end
 function HaskellNeotestAdapter.discover_positions(path)
   local query = [[
   ;; describe
-  (exp_apply
+  ((exp_apply
     (exp_name (variable) @func_name) (#match? @func_name "^describe$")
-    (exp_literal) @test_name
-  ) @test.definition
+    (exp_literal) @test.description
+  )) @test.definition
   ;; it
-  (exp_apply
+  ((exp_apply
     (exp_name (variable) @func_name) (#match? @func_name "^it$")
-    (exp_literal) @test_name
-  ) @test.definition
+    (exp_literal) @test.description
+  )) @test.definition
   ;; prop
-  (exp_apply
+  ((exp_apply
     (exp_name (variable) @func_name) (#match? @func_name "^prop$")
-    (exp_literal) @test_name
+    (exp_literal) @test.description
+  ) @test.definition
+  ;; describe (qualified)
+  (exp_apply
+    (exp_name (qualified_variable (variable) @func_name)) (#match? @func_name "^describe$")
+    (exp_literal) @test.description
+  ) @test.definition
+  ;; it (qualified)
+  (exp_apply
+    (exp_name (qualified_variable (variable) @func_name)) (#match? @func_name "^it$")
+    (exp_literal) @test.description
+  ) @test.definition
+  ;; prop (qualified)
+  (exp_apply
+    (exp_name (qualified_variable (variable) @func_name)) (#match? @func_name "^prop$")
+    (exp_literal) @test.description
   ) @test.definition
   ]]
   local result = lib.treesitter.parse_positions(path, query, { nested_namespaces = true })
-  print(vim.inspect(result))
+  -- print(vim.inspect(result))
   return result
 end
 
@@ -48,7 +63,19 @@ end
 ---@param args neotest.RunArgs
 ---@return neotest.RunSpec
 function HaskellNeotestAdapter.build_spec(args)
-  print("DEBUG: Building spec: " .. vim.inspect(args))
+  print("[DEBUG] Building spec...")
+  -- local results_path = vim.fn.tempname()
+  local tree = args.tree
+  if not tree then
+    return
+  end
+  -- print("[DEBUG] Tree: " .. vim.inspect(tree))
+  local pos = args.tree:data()
+  if pos.type == "dir" then
+    return
+  end
+  print("[DEBUG] pos type: " .. vim.inspect(pos.type))
+  -- local filters = {}
   -- TODO
   return {}
 end
