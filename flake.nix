091d2da503a052e213013537f450ae78326d2a5f
Marc Jakobi <mrcjkb89@outlook.com>
2023-01-02 21:03:11 +0100

    style: replace nixpkgs-fmt with alejandra

--- flake.nix
+++ flake.nix
@@ -30,76 +30,86 @@
     };
   };
 
-  outputs = inputs@{ self, nixpkgs, pre-commit-hooks, packer-nvim, plenary-nvim, neotest, ... }:
-    let
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    pre-commit-hooks,
+    packer-nvim,
+    plenary-nvim,
+    neotest,
+    ...
+  }: let
+    name = "neotest-haskell";
 
-      name = "neotest-haskell";
+    supportedSystems = [
+      "aarch64-linux"
+      "aarch64-darwin"
+      "x86_64-darwin"
+      "x86_64-linux"
+    ];
+    perSystem = nixpkgs.lib.genAttrs supportedSystems;
+    pkgsFor = system: import nixpkgs {inherit system;};
 
-      supportedSystems = [
-        "aarch64-linux"
-        "aarch64-darwin"
-        "x86_64-darwin"
-        "x86_64-linux"
-      ];
-      perSystem = nixpkgs.lib.genAttrs supportedSystems;
-      pkgsFor = system: import nixpkgs { inherit system; };
+    ci-overlay = import ./nix/ci-overlay.nix {inherit (inputs) packer-nvim plenary-nvim neotest;};
+    nvim-plugin-overlay = import ./nix/nvim-plugin-overlay.nix {
+      inherit name;
+      self = ./.;
+    };
 
-      ci-overlay = import ./nix/ci-overlay.nix { inherit (inputs) packer-nvim plenary-nvim neotest; };
-      nvim-plugin-overlay = import ./nix/nvim-plugin-overlay.nix { inherit name; self = ./.; };
+    nvim-plugin-for = system: let
+      pkgs = pkgsFor system;
+    in
+      pkgs.vimUtils.buildVimPluginFrom2Nix {
+        inherit name;
+        src = ./.;
+      };
 
-      nvim-plugin-for = system:
-        let
-          pkgs = pkgsFor system;
-        in
-        pkgs.vimUtils.buildVimPluginFrom2Nix {
-          inherit name;
-          src = ./.;
-        };
-
-      pre-commit-check-for = system: pre-commit-hooks.lib.${system}.run {
+    pre-commit-check-for = system:
+      pre-commit-hooks.lib.${system}.run {
         src = ./.;
         hooks = {
-          nixpkgs-fmt.enable = true;
+          alejandra.enable = true;
           stylua.enable = true;
         };
       };
 
-      shellFor = system:
-        let
-          pkgs = pkgsFor system;
-          pre-commit-check = pre-commit-check-for system;
-        in
-        pkgs.mkShell {
-          name = "neotest-haskell devShell";
-          inherit (pre-commit-check) shellHook;
-          buildInputs = with pkgs; [
-            zlib
-          ];
-        };
+    shellFor = system: let
+      pkgs = pkgsFor system;
+      pre-commit-check = pre-commit-check-for system;
     in
-    {
-      overlays = {
-        inherit ci-overlay nvim-plugin-overlay;
-        default = nvim-plugin-overlay;
+      pkgs.mkShell {
+        name = "neotest-haskell devShell";
+        inherit (pre-commit-check) shellHook;
+        buildInputs = with pkgs; [
+          zlib
+          stylua
+          alejandra
+        ];
       };
-
-      devShells = perSystem (system: rec {
-        default = devShell;
-        devShell = shellFor system;
-      });
-
-      packages = perSystem (system: rec {
-        default = nvim-plugin;
-        nvim-plugin = nvim-plugin-for system;
-      });
-
-      checks = perSystem (system:
-        let
-          checkPkgs = import nixpkgs { inherit system; overlays = [ ci-overlay ]; };
-        in
-        {
-          formatting = pre-commit-check-for system;
-          inherit (checkPkgs) ci;
-        });
+  in {
+    overlays = {
+      inherit ci-overlay nvim-plugin-overlay;
+      default = nvim-plugin-overlay;
     };
+
+    devShells = perSystem (system: rec {
+      default = devShell;
+      devShell = shellFor system;
+    });
+
+    packages = perSystem (system: rec {
+      default = nvim-plugin;
+      nvim-plugin = nvim-plugin-for system;
+    });
+
+    checks = perSystem (system: let
+      checkPkgs = import nixpkgs {
+        inherit system;
+        overlays = [ci-overlay];
+      };
+    in {
+      formatting = pre-commit-check-for system;
+      inherit (checkPkgs) ci;
+    });
+  };
 }
