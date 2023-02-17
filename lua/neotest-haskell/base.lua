65418e891ac804829b7175a99f3f7bb6ceb98394
Marc Jakobi <mrcjkb89@outlook.com>
2023-02-18 00:49:31 +0100

    chore: add some emmylua

--- lua/neotest-haskell/base.lua
+++ lua/neotest-haskell/base.lua
@@ -2,6 +2,8 @@ local lib = require('neotest.lib')
 
 local base = {}
 
+---@param file_path string
+---@return boolean
 base.is_test_file = function(file_path)
   if not vim.endswith(file_path, '.hs') then
     return false
@@ -9,12 +11,16 @@ base.is_test_file = function(file_path)
   return vim.endswith(file_path, 'Spec.hs') or vim.endswith(file_path, 'Test.hs') or vim.endswith(file_path, 'Tests.hs')
 end
 
+---@param name string
+---@return boolean
 base.filter_dir = function(name)
   return name ~= 'dist-newstile' and name ~= '.stack-work'
 end
 
+---@type fun(file:string):(string|nil)
 base.match_package_root_pattern = lib.files.match_root_pattern('*.cabal', 'package.yaml')
 
+---@type fun(file:string):(string|nil)
 base.match_project_root_pattern = lib.files.match_root_pattern('cabal.project', 'stack.yaml')
 
 return base
