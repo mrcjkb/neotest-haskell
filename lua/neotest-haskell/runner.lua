0f9a5d6ab5ab646d4836fdda63ff5d2d26f790ec
Marc Jakobi <mrcjkb89@outlook.com>
2023-03-11 17:36:45 +0100

    docs: update runner emmylua

--- lua/neotest-haskell/runner.lua
+++ lua/neotest-haskell/runner.lua
@@ -5,8 +5,9 @@ local Path = require('plenary.path')
 
 local runner = {}
 
----@param directory string
----@param patterns string[]
+---Check if the given directory contains a file matching a list of patterns.
+---@param directory string The directory to check for.
+---@param patterns string[] The patterns to check for.
 ---@return boolean
 local function directory_contains_file_matching(directory, patterns)
   for _, pattern in ipairs(patterns) do
@@ -22,8 +23,8 @@ end
 ---Attempts to determine the package name.
 ---If a *.cabal is present, this is *.
 ---Otherwise, we assume the package name is the same as the directory name.
----@param package_root string The package root directory
----@return string package_name The assumed package name
+---@param package_root string The package root directory.
+---@return string package_name The assumed package name.
 local function get_package_name(package_root)
   for _, package_file_path in ipairs(async.fn.glob(Path:new(package_root, '*.cabal').filename, true, true)) do
     local package_file_name = vim.fn.fnamemodify(package_file_path, ':t')
@@ -37,10 +38,10 @@ local function get_package_name(package_root)
 end
 
 ---Select a build tool from the given list of build tools preferring the first that can be used.
----@param test_file_path string A test file in a project
----@param build_tools build_tool[] List of build tools to choose from
----@return fun(neotest.Tree):neotest.RunSpec mk_command A function that builds the runner command for a test tree
-runner.select_build_tool = function(test_file_path, build_tools)
+---@param test_file_path string A test file in a project.
+---@param build_tools build_tool[] List of build tools to choose from.
+---@return fun(neotest.Tree):neotest.RunSpec mk_command A function that builds the runner command using the selected build tool for a test tree.
+function runner.select_build_tool(test_file_path, build_tools)
   -- A package always has a *.cabal file (or in rare cases just a package.yaml file).
   local package_root = lib.files.match_root_pattern('*.cabal', 'package.yaml')(test_file_path)
   if not package_root then
